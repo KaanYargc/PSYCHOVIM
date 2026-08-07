b03c2490aecad9bd1ea409b2d8a456c1e46b892b
Onur <51912619+OnurByte@users.noreply.github.com>
2026-08-08 02:14:00 +0300

    fix: load persistent Pycho settings before keymaps

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
 require("psychovim.options")
+require("psychovim.settings").setup()
 require("psychovim.keymaps")
 require("psychovim.autocmds")
 require("psychovim.lazy")
