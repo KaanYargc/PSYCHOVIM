d79431c57a1eddb4fb002205f4013d5b1ea9f434
Onur <51912619+OnurByte@users.noreply.github.com>
2026-08-08 03:07:49 +0300

    feat: wire settings marketplace and updater controls

--- lua/psychovim/features.lua
+++ lua/psychovim/features.lua
@@ -12,6 +12,9 @@ function M.setup()
   local dorsia = require("psychovim.dorsia")
   local routine = require("psychovim.routine")
   local settings = require("psychovim.settings")
+  local marketplace = require("psychovim.marketplace")
+  local updater = require("psychovim.updater")
+  local default_editor = require("psychovim.default_editor")
   local actions = require("psychovim.pycho_actions")
   local close = require("psychovim.pycho_close")
 
@@ -19,11 +22,14 @@ function M.setup()
   card.setup()
   dorsia.setup()
   routine.setup()
+  marketplace.setup()
+  updater.setup()
   theme.apply(vim.g.psychovim_mask or "sanity")
 
   command("PychoSave", actions.save, "Save the current buffer")
   command("PychoClose", close.open, "Open PychoClose")
   command("PychoBufferClose", actions.close_buffer, "Close the current buffer safely")
+  command("PychoMakeDefault", function() default_editor.make_default() end, "Make Pycho the default text editor")
 
   local map = vim.keymap.set
   map("n", "<leader>pb", card.open, { desc = "Pycho Business Card" })
@@ -32,7 +38,17 @@ function M.setup()
   map("n", "<leader>pd", dorsia.open, { desc = "Pycho Dorsia" })
   map("n", "<leader>pS", dorsia.save, { desc = "Pycho Dorsia Save" })
   map("n", "<leader>px", dorsia.forget, { desc = "Pycho Dorsia Forget" })
-  map("n", "<leader>ps", settings.open, { desc = "Pycho Settings" })
+  map("n", "<leader>ps", settings.open, { desc = "⚙ Pycho Settings" })
+  map("n", "<leader>pp", marketplace.open, { desc = "󰏗 Pycho Marketplace" })
+  map("n", "<leader>pu", updater.run, { desc = "pychoUpdater" })
+
+  vim.api.nvim_create_autocmd("VimEnter", {
+    group = vim.api.nvim_create_augroup("PychoDefaultEditorWatch", { clear = true }),
+    once = true,
+    callback = function()
+      if vim.env.PSYCHOVIM_MAINTENANCE ~= "1" then default_editor.warn_if_changed() end
+    end,
+  })
 end
 
 return M
